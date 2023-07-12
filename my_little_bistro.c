5a7b5435dcb1ccb7690d40bff6a5c817498c879c
mlagie <139330130+mlagie@users.noreply.github.com>
2023-07-12 12:52:18 +0000

    [add] my_little_bistro

---
+++ my_little_bistro.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int my_little_bistro(int value1, char op, int value2){
+    if(op == '+') return value1 + value2;
+    else if(op == '-') return value1 - value2;
+    else if(op == '*') return value1 * value2;
+    else if(op == '%') return value1 % value2;
+    else if(op == '/') return value1 / value2;
+}
+
