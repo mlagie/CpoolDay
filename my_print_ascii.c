a7c5753c8761f29d6388fb321bcdf4ecdb5d6520
mlagie <139330130+mlagie@users.noreply.github.com>
2023-07-12 09:38:09 +0000

    [ADD] my_print_ASCII

--- my_print_ascii.c
+++ my_print_ascii.c
@@ -1,5 +1,12 @@
 void print_char(char c);
 
+void my_print_ascii(void){
+    for(int c = 33; c <= 126; ++c){
+        print_char(c);
+    }
+}
+
 int main(void){
-    print_char("test");
+    my_print_ascii();
+    return 0;
 }
