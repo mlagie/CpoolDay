738ae2c0ed1bfcfc862d38667620295108f36d87
mlagie <139330130+mlagie@users.noreply.github.com>
2023-07-12 10:13:49 +0000

    [add] my_print_ascii

--- my_print_n_ascii.c
+++ my_print_n_ascii.c
@@ -1,6 +1,8 @@
 void print_char(char c);
 
 void my_print_n_acsii(int howMany){
+    if(howMany < 0 || howMany > 94) return;
+
     for(int i = 33; i < (33 + howMany); i++)
     {
         print_char(i);
